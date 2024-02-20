200ca2953a7f3346d5e859150ea6311a4cb401af
MrSteelRat <drebin341@gmail.com>
2024-02-21 01:08:12 +0300

    Start Module FileSateModule(WIP)

--- module/FileStateModule.cpp
+++ module/FileStateModule.cpp
@@ -2,4 +2,23 @@
 // Created by Mr Steel Rat on 2/20/2024.
 //
 
+#include <iostream>
+
 #include "FileStateModule.h"
+
+
+
+size_t FileStateModule::creave_env_var(std::unique_ptr<std::string> name_file, std::unique_ptr<std::string> value_file)
+{
+    size_t setup_env = setenv(name_file->c_str(), value_file->c_str(), 1);
+    if(setup_env == 0) {
+        std::cout << "Environment variable [" << *name_file << "] set successfully with value [" << *value_file << std::endl;
+    }
+    else {
+        std::cout << "Failed to set environment variable [" << *name_file << "] with value [" << *value_file << "]";
+    }
+
+    std::cout.flush();
+    
+    return setup_env;
+}
--- module/FileStateModule.h
+++ module/FileStateModule.h
@@ -5,10 +5,16 @@
 #ifndef FILESTATEMODULE_H
 #define FILESTATEMODULE_H
 
+#include <iostream>
+#include <memory>
+
+#include <tgbot/tgbot.h>
 
 
 class FileStateModule {
-
+    public:
+        size_t creave_env_var(std::unique_ptr<std::string> name, std::unique_ptr<std::string> value);
+        void send_message_to_admin(unsigned long long int chat_id, std::string * env_var_name, TgBot::Bot * bot, std::string *);
 };
 
 
